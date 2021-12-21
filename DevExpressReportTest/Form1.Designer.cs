8415e58c9ff08bffc47e5433bf6234ecc9b1a134
hmji <hmji@nansoft.co.kr>
2021-12-21 15:30:40 +0900

    Add project files.

---
+++ DevExpressReportTest/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace DevExpressReportTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(30, 31);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(132, 61);
+            this.btnPrint.TabIndex = 0;
+            this.btnPrint.Text = "출력";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 633);
+            this.Controls.Add(this.btnPrint);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrint;
+    }
+}
+
---
+++ DevExpressReportTest/Form1.cs
@@ -0,0 +1,35 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DevExpressReportTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            XtraReport report = XtraReport.FromFile(@"..\\..\\Report11.repx", true);
+            using (ReportPrintTool printTool = new ReportPrintTool(report))
+            {
+                printTool.PrintDialog();
+                
+                // Send the report to the default printer.
+                printTool.Print();
+
+                // Send the report to the specified printer.
+                //printTool.Print("myPrinter");
+            }
+        }
+    }
+}
