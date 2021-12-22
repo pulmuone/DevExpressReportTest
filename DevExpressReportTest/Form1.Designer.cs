caf9d524748b8bcab7542ab7b286793319f3fb08
hmji <hmji@nansoft.co.kr>
2021-12-22 17:54:03 +0900

    test

--- DevExpressReportTest/Form1.Designer.cs
+++ DevExpressReportTest/Form1.Designer.cs
@@ -30,11 +30,16 @@ namespace DevExpressReportTest
         private void InitializeComponent()
         {
             this.btnPrint = new System.Windows.Forms.Button();
+            this.btnPrinterSelect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSubReport = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnPrint
             // 
-            this.btnPrint.Location = new System.Drawing.Point(30, 31);
+            this.btnPrint.Location = new System.Drawing.Point(30, 106);
             this.btnPrint.Name = "btnPrint";
             this.btnPrint.Size = new System.Drawing.Size(132, 61);
             this.btnPrint.TabIndex = 0;
@@ -42,21 +47,76 @@ namespace DevExpressReportTest
             this.btnPrint.UseVisualStyleBackColor = true;
             this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
             // 
+            // btnPrinterSelect
+            // 
+            this.btnPrinterSelect.Location = new System.Drawing.Point(211, 106);
+            this.btnPrinterSelect.Name = "btnPrinterSelect";
+            this.btnPrinterSelect.Size = new System.Drawing.Size(107, 61);
+            this.btnPrinterSelect.TabIndex = 1;
+            this.btnPrinterSelect.Text = "프린터 선택";
+            this.btnPrinterSelect.UseVisualStyleBackColor = true;
+            this.btnPrinterSelect.Click += new System.EventHandler(this.btnPrinterSelect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(522, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(8, 249);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(791, 374);
+            this.panel1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(29, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(522, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // btnSubReport
+            // 
+            this.btnSubReport.Location = new System.Drawing.Point(459, 106);
+            this.btnSubReport.Name = "btnSubReport";
+            this.btnSubReport.Size = new System.Drawing.Size(111, 61);
+            this.btnSubReport.TabIndex = 6;
+            this.btnSubReport.Text = "서브리포트";
+            this.btnSubReport.UseVisualStyleBackColor = true;
+            this.btnSubReport.Click += new System.EventHandler(this.btnSubReport_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(810, 633);
+            this.Controls.Add(this.btnSubReport);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnPrinterSelect);
             this.Controls.Add(this.btnPrint);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnPrinterSelect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnSubReport;
     }
 }
 
--- DevExpressReportTest/Form1.cs
+++ DevExpressReportTest/Form1.cs
@@ -1,21 +1,27 @@
-﻿using DevExpress.XtraBars.Docking;
+﻿using DevExpress.LookAndFeel;
+using DevExpress.XtraBars.Docking;
 using DevExpress.XtraPrinting;
 using DevExpress.XtraPrinting.Control;
 using DevExpress.XtraPrinting.Preview;
 using DevExpress.XtraReports.UI;
+using DevExpressReportTest.Properties;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using WindowsFormsApp5.Printer;
 
 namespace DevExpressReportTest
 {
     public partial class Form1 : Form
     {
+        DataSetReport dsReport = new DataSetReport();
         public Form1()
         {
             InitializeComponent();
@@ -23,22 +29,52 @@ namespace DevExpressReportTest
 
         private void btnPrint_Click(object sender, EventArgs e)
         {
-            DataSetReport dsReport = new DataSetReport();
+            
             DataRow dr;
 
-            for (int i = 0; i < 10; i++)
-            {
-                dr = dsReport.Tables[0].NewRow();
-                
-                dr["BARCODE"] = "12345" + i.ToString();
-                dr["PRODUCT_NAME"] ="상품이름 " + i.ToString();
-                dr["CD_SIZE"] = "컬러 " + i.ToString();
-                dr["CD_COLOR"] = "사이즈 " + i.ToString();
+            dr = dsReport.Tables[0].NewRow();
+            dr["BARCODE"] = "11111";
+            dr["PRODUCT_NAME"] = "상품이름1";
+            dr["CD_SIZE"] = "컬러1";
+            dr["CD_COLOR"] = "사이즈1";
+            dsReport.Tables[0].Rows.Add(dr);
 
-                dsReport.Tables[0].Rows.Add(dr);
-            }
+            dr = dsReport.Tables[0].NewRow();
+            dr["BARCODE"] = "11111";
+            dr["PRODUCT_NAME"] = "상품이름2";
+            dr["CD_SIZE"] = "컬러2";
+            dr["CD_COLOR"] = "사이즈2";
+            dsReport.Tables[0].Rows.Add(dr);
 
-            XtraReport report = XtraReport.FromFile(@"..\\..\\Report11.repx", true);
+            dr = dsReport.Tables[0].NewRow();
+            dr["BARCODE"] = "22222";
+            dr["PRODUCT_NAME"] = "상품이름3";
+            dr["CD_SIZE"] = "컬러3";
+            dr["CD_COLOR"] = "사이즈3";
+            dsReport.Tables[0].Rows.Add(dr);
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    dr = dsReport.Tables[0].NewRow();
+
+            //    dr["BARCODE"] = "12345" + i.ToString();
+            //    dr["PRODUCT_NAME"] ="상품이름 " + i.ToString();
+            //    dr["CD_SIZE"] = "컬러 " + i.ToString();
+            //    dr["CD_COLOR"] = "사이즈 " + i.ToString();
+
+            //    dsReport.Tables[0].Rows.Add(dr);
+            //}
+
+            //XtraReport report = XtraReport.FromFile("Report11.repx", true);
+            XtraReport report = XtraReport.FromFile(Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location), "Report11.repx"), true);
+            //XtraReport report = new XtraReport();
+            //report.LoadLayout(@"..\\..\\Report11.repx");
+            //report.LoadLayout(@"Report11.repx");
+
+            XRSubreport subReport = new XRSubreport();
+            subReport.ReportSourceUrl  = Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location), "ReportSub.repx");
+
+            subReport.BeforePrint += SubReport_BeforePrint;
 
             report.DataSource = dsReport;
 
@@ -51,24 +87,129 @@ namespace DevExpressReportTest
             report.Parameters["whcd"].Visible = false;
             report.Parameters["orderType"].Visible = false;
             report.Parameters["slipno"].Visible = false;
-
+                       
+            
             using (ReportPrintTool printTool = new ReportPrintTool(report))
             {
-                printTool.PrintDialog();
+                printTool.PrintingSystem.StartPrint += new PrintDocumentEventHandler(PrintingSystem_StartPrint);
+                printTool.PrintingSystem.PrintProgress += new PrintProgressEventHandler(PrintingSystem_PrintProgress);
+                printTool.PrintingSystem.EndPrint += new EventHandler(PrintingSystem_EndPrint);
+                                
+                //printTool.PrintDialog();
 
                 // Send the report to the default printer.
-                printTool.Print();
+                //printTool.Print();
 
                 //printTool.PrintingSystem.StartPrint += PrintingSystem_StartPrint;
                 //Send the report to the specified printer.
-                //printTool.Print("myPrinter");
+                printTool.Print(this.textBox1.Text); //프린터 지정해서 바로 인쇄
             }
+        }
+
+        private void SubReport_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            ((XRSubreport)sender).ReportSource.DataSource = dsReport;
+            ((XRSubreport)sender).ReportSource.DataMember = "DataTableReport";
+        }
+
+        private void PrintingSystem_EndPrint(object sender, EventArgs e)
+        {
+            Debug.WriteLine("프린터 종료");
+        }
+
+        private void PrintingSystem_PrintProgress(object sender, PrintProgressEventArgs e)
+        {
+            Debug.WriteLine(string.Format("{0} : {1}", "프린터 인쇄 중", e.PageIndex));
+            Console.WriteLine(e.PrintAction);
+        }
+
+        private void Report_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+
         }
 
         private void PrintingSystem_StartPrint(object sender, PrintDocumentEventArgs e)
         {
-            e.PrintDocument.PrinterSettings.FromPage = 1;
-            e.PrintDocument.PrinterSettings.ToPage = 10;
+            //e.PrintDocument.PrinterSettings.FromPage = 1;
+            //e.PrintDocument.PrinterSettings.ToPage = 10;
+
+            //prnSettings = e.PrintDocument.PrinterSettings;
+            Debug.WriteLine("프린터 시작");
+        }
+
+        private void btnPrinterSelect_Click(object sender, EventArgs e)
+        {
+            PrinterSelect printerSelect = new PrinterSelect();
+            printerSelect.PrinaterSelectEvent += PrinterName;
+
+            printerSelect.ShowDialog();
+        }
+
+        private void PrinterName(string printerName)
+        {
+            this.textBox1.Text = printerName;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.textBox1.Text = Settings.Default.PrinterName;
+            this.textBox2.Text = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+        }
+
+        private void btnSubReport_Click(object sender, EventArgs e)
+        {
+
+            DataRow dr;
+
+            dr = dsReport.Tables[0].NewRow();
+            dr["BARCODE"] = "11111";
+            dr["PRODUCT_NAME"] = "상품이름1";
+            dr["CD_SIZE"] = "컬러1";
+            dr["CD_COLOR"] = "사이즈1";
+            dsReport.Tables[0].Rows.Add(dr);
+
+            dr = dsReport.Tables[0].NewRow();
+            dr["BARCODE"] = "11111";
+            dr["PRODUCT_NAME"] = "상품이름2";
+            dr["CD_SIZE"] = "컬러2";
+            dr["CD_COLOR"] = "사이즈2";
+            dsReport.Tables[0].Rows.Add(dr);
+
+            dr = dsReport.Tables[0].NewRow();
+            dr["BARCODE"] = "22222";
+            dr["PRODUCT_NAME"] = "상품이름3";
+            dr["CD_SIZE"] = "컬러3";
+            dr["CD_COLOR"] = "사이즈3";
+            dsReport.Tables[0].Rows.Add(dr);
+
+            XtraReport report = new XtraReport();
+            XtraReport subreport = new XtraReport();
+
+            report.LoadLayout(Application.StartupPath + @"\Report11.repx");
+            subreport.LoadLayout(Application.StartupPath + @"\ReportSub.repx");
+            ((XRSubreport)report.FindControl("subreport1", true)).ReportSource = subreport;
+            ((XRSubreport)report.FindControl("subreport1", true)).BeforePrint += new System.Drawing.Printing.PrintEventHandler(Form1_BeforePrint);
+
+            report.DataSource = dsReport;
+
+            report.Parameters["orderDate"].Value = "2021-12-21";
+            report.Parameters["whcd"].Value = "WH001";
+            report.Parameters["orderType"].Value = "발주";
+            report.Parameters["slipno"].Value = "1234567890";
+
+            using (ReportPrintTool printTool = new ReportPrintTool(report))
+            {
+                printTool.Print(this.textBox1.Text); //프린터 지정해서 바로 인쇄
+            }
+
+            //report를 직접 Print하는 것과 ReportPrintTool로 하는것의 차이점
+            //report.Print(this.textBox1.Text); //프린터 지정해서 바로 인쇄
+        }
+
+        void Form1_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            ((XRSubreport)sender).ReportSource.DataSource = dsReport;
+            //((XRSubreport)sender).ReportSource.FilterString = "[CategoryID] = " + Convert.ToInt32(((XRSubreport)sender).Report.GetCurrentColumnValue("CategoryID")).ToString();
         }
     }
 }
